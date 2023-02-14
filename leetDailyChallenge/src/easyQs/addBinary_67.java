5971e785185d2980e2c00d5e1fd6cbe1668e6bdf
Amar Prasad <amarprasad77354@gmail.com>
2023-02-14 21:08:37 +0530

    daily challenge

---
+++ leetDailyChallenge/src/easyQs/addBinary_67.java
@@ -0,0 +1,24 @@
+package easyQs;
+
+public class addBinary_67 {
+    public static void main(String[] args) {
+        String an = "101";
+        int a = (an.charAt(0) -'0') + 1;
+        System.out.println(a);
+    }
+
+    public static String addBinary(String a, String b) {
+        //edge cases
+        char[] ar = a.toCharArray();
+        char[] br = b.toCharArray();
+        String ans = "";
+        int temp = 0;
+        int cur = 0;
+        int i=ar.length-1, j=br.length-1;
+        while (i> 0 && j >0){
+
+        }
+
+        return ans;
+    }
+}
