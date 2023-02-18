bd5a0fe8dbbb0363fcd9d2d853505098b9a45908
Amar Prasad <amarprasad77354@gmail.com>
2023-02-18 19:57:42 +0530

    qs

---
+++ Stack&Queues/src/com/LKcoder/QuesOnStackAndQueues/validParenthesis.java
@@ -0,0 +1,39 @@
+package com.LKcoder.QuesOnStackAndQueues;
+
+import java.util.*;
+
+public class validParenthesis {
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch: s.toCharArray()){
+            if (ch == '(' || ch == '{' || ch == '[' ){
+                stack.push(ch);
+            } else {
+                if (ch == ')'){
+                    if (stack.isEmpty() || stack.pop() != '('){
+                        return false;
+                    }
+                }
+                if (ch == '}'){
+                    if (stack.isEmpty() || stack.pop() != '{' ){
+                        return false;
+                    }
+                }
+                if (ch == ']'){
+                    if (stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
