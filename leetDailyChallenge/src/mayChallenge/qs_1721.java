cf3a25e38e55f1a0f8fc647adc251317546c4f9d
Amar Prasad <amarprasad77354@gmail.com>
2023-05-15 22:53:07 +0530

    dc

---
+++ leetDailyChallenge/src/mayChallenge/qs_1721.java
@@ -0,0 +1,60 @@
+package mayChallenge;
+import java.util.*;
+public class qs_1721 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        swapNodes(head, 2);
+        ListNode c = head;
+
+        while (c != null){
+            System.out.print(c.val + "-->");
+            c = c.next;
+        }
+    }
+
+    public static ListNode swapNodes(ListNode head, int k) {
+        ListNode temp = head;
+        int n =1, i = 1;
+        while(temp != null){
+            temp = temp.next;
+            n++;
+        }
+        n -= 1;
+        ListNode firstPrev = head; ListNode curr = head;
+        ListNode firstNext = head;
+        ListNode secSwap = head;
+        while(curr != null){
+            // swapping the nodes
+            if(n-k == i){
+                secSwap = curr.next;
+                temp.next = curr.next.next;
+                curr.next = temp;
+                secSwap.next = firstNext;
+                firstPrev.next = secSwap;
+                break;
+            }
+            if(i == k-1){
+                temp = curr.next;
+                firstPrev = curr;
+                firstNext = curr.next.next;
+            }
+            curr = curr.next;
+            i++;
+        }
+        return head;
+    }
+}
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
