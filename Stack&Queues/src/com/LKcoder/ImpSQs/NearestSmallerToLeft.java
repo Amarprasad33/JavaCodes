17af94738dfe0c92471fb91c6bfc202e5439b9ac
Amar Prasad <amarprasad77354@gmail.com>
2023-06-27 19:18:58 +0530

    dp stack

--- Stack&Queues/src/com/LKcoder/ImpSQs/NearestSmallerToLeft.java
+++ Stack&Queues/src/com/LKcoder/ImpSQs/NearestSmallerToLeft.java
@@ -1,6 +1,7 @@
 package com.LKcoder.ImpSQs;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -8,7 +9,7 @@ public class NearestSmallerToLeft {
     public static void main(String[] args) {
         int[] arr = {4, 5, 2, 10, 8};
         int n = 5;
-        System.out.println(NearestSmallerToLeft(arr, n));
+        System.out.println(NearestSmallerToRight(arr, n));
     }
 
 
@@ -42,5 +43,38 @@ public class NearestSmallerToLeft {
     }
 
     // OUTPUT: -1, 4, -1, 2, 2
+
+
+
+    public static List<Integer> NearestSmallerToRight(int[] arr, int n){
+        Stack<Integer> stk = new Stack<>();
+        List<Integer> ls = new ArrayList<>();
+
+        for (int i = n-1; i>=0; i--){
+            // If stack is empty in the start
+            if(stk.size() == 0)
+                ls.add(-1);
+            else if (stk.size() > 0 && stk.peek() < arr[i]) {   // Greater element will be at top
+                ls.add(stk.peek());
+            } else if (stk.size() > 0 && stk.peek() >= arr[i]) {
+//               // if the top element is larger, then pop until it empties or top element is smaller than arr[i]
+                while (stk.size() > 0 && stk.peek() >= arr[i]){
+                    stk.pop();
+                }
+                // the loop will end because of one of the two conditions
+                if (stk.size() == 0){
+                    // it means the stack is empty and there is no smaller element to the right
+                    ls.add(-1);
+                }else {
+                    // smaller element lies in top
+                    ls.add(stk.peek());
+                }
+            }
+            stk.push(arr[i]);
+        }
+        Collections.reverse(ls);
+        return ls;
+    }
+
 
 }
