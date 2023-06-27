602514d865392126a35a21deb663b6ccae3b997f
Amar Prasad <amarprasad77354@gmail.com>
2023-06-27 18:40:51 +0530

    stac dp

--- Stack&Queues/src/com/LKcoder/ImpSQs/NearestGreaterToRight.java
+++ Stack&Queues/src/com/LKcoder/ImpSQs/NearestGreaterToRight.java
@@ -9,7 +9,7 @@ public class NearestGreaterToRight {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 4};
         int n = 4;
-        System.out.println(NGT(arr, n));
+        System.out.println(NearestGreaterToLeft(arr, n));
     }
 
     // Optimal approach to solve it using stacks
@@ -40,6 +40,36 @@ public class NearestGreaterToRight {
             stk.push(arr[i]);
         }
         Collections.reverse(ls);
+        return ls;
+    }
+
+
+    public static List<Integer> NearestGreaterToLeft(int[] arr, int n){
+        Stack<Integer> stk = new Stack<>();
+        List<Integer> ls = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            // If stack is empty in the start
+            if(stk.size() == 0)
+                ls.add(-1);
+            else if (stk.size() > 0 && stk.peek() > arr[i]) {   // Greater element will be at top
+                ls.add(stk.peek());
+            } else if (stk.size() > 0 && stk.peek() <= arr[i]) {
+//               // if the top element is smaller, then pop until it empties or top element is greater than arr[i]
+                while (stk.size() > 0 && stk.peek() <= arr[i]){
+                    stk.pop();
+                }
+                // the loop will end because of one of the two conditions
+                if (stk.size() == 0){
+                    // it means the stack is empty and there is no greater element to the left
+                    ls.add(-1);
+                }else {
+                    // greater element lies in top
+                    ls.add(stk.peek());
+                }
+            }
+            stk.push(arr[i]);
+        }
         return ls;
     }
 
