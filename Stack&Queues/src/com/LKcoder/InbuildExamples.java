edd9674949c4322e3327ab7282b21c2b9f2d2ec2
Amar Prasad <amarprasad77354@gmail.com>
2023-06-30 23:06:06 +0530

    dp pra

--- Stack&Queues/src/com/LKcoder/InbuildExamples.java
+++ Stack&Queues/src/com/LKcoder/InbuildExamples.java
@@ -30,11 +30,36 @@ public class InbuildExamples {
 //        System.out.println(queue.remove());
 //
 
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.add(690);
-        deque.addLast(55);
-        deque.addFirst(4);
+//        Deque<Integer> deque = new ArrayDeque<>();
+//        deque.add(690);
+//        deque.addLast(55);
+//        deque.addFirst(4);
+//        System.out.println(deque);
         // etc etc
 
+        int[] nums = {-1,1,0,-3,3};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+
     }
+
+    public static int[] productExceptSelf(int[] nums) {
+        if(nums.length < 1){
+            return  new int[]{};
+        }
+
+        int[] ans = new int[nums.length];
+        int k = 0, prod = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i!=j){
+                    prod *= nums[j];
+                }
+            }
+            ans[i] = prod;
+            prod = 1;
+        }
+        return ans;
+    }
+
+    
 }
