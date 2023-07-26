04f2f230712381b71dffd3ab5c4d77337ca50daa
Amar Prasad <amarprasad77354@gmail.com>
2023-07-26 11:18:17 +0530

    per_case

--- Recursion/src/com/AVProblems/permutationWithSpaces.java
+++ Recursion/src/com/AVProblems/permutationWithSpaces.java
@@ -1,13 +1,21 @@
 package com.AVProblems;
 
+import java.util.Collections;
+
 public class permutationWithSpaces {
     public static void main(String[] args) {
-        String ip = "ABC";
+//        String ip = "ABC";
+//        String op = "";
+//        op += ip.charAt(0);
+//        ip = ip.substring(1);
+//        System.out.println(ip + "-------------" + op);
+//        solvePermutationAV(op, ip);
+
+        String ip = "ab";
         String op = "";
-        op += ip.charAt(0);
-        ip = ip.substring(1);
-        System.out.println(ip + "-------------" + op);
-        solvePermutationAV(op, ip);
+        PermutationCase(op, ip);
+
+
     }
 
     private static void solvePermutation(String op, String ip) {
@@ -29,5 +37,21 @@ public class permutationWithSpaces {
         String op2 = op;
         solvePermutationAV(op1 + "-" + ip.charAt(0), ip.substring(1));
         solvePermutationAV(op2 + ip.charAt(0), ip.substring(1));
+    }
+
+    // Below codes are for permutation with case change:
+    // [a,b] ==> ab, Ab, aB, AB
+    public static void PermutationCase(String op, String ip) {
+        if (ip.isEmpty()){
+            System.out.println(op);
+            return;
+        }
+        String op1 = op;
+        String op2 = op;
+        char ch = ip.charAt(0);
+        op1 += Character.toUpperCase(ch);
+        op2 += ip.charAt(0);
+        PermutationCase(op1, ip.substring(1));
+        PermutationCase(op2, ip.substring(1));
     }
 }
